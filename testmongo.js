8ba71f8c2ec43828f3750e9a5ff2678cc043f0ad
Nick Solari <91080258+nicksolari79@users.noreply.github.com>
2026-03-30 22:55:36 -0500

    Update MongoDB connection and collection names

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<user>:<password>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://nicksolari79_db_user:DGj196QYfAQyCZBr@415.czu6hlw.mongodb.net/?appName=415";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
@@ -33,8 +33,8 @@ console.log("Looking for: " + searchKey);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+    const database = client.db('CMPS415');
+    const parts = database.collection('mycollection');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
